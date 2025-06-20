78d24e371b9d7395e7777c5ca1336b3e1801a9a5
XawiiQR <qjavier617@gmail.com>
2025-06-19 22:09:46 -0500

    commit

--- relu.cpp
+++ relu.cpp
@@ -1,1 +1,6 @@
 #include "relu.h"
+Tensor ReLU::forward(const Tensor& input) const {
+    Tensor output(input.depth, input.height, input.width);
+    for (size_t i = 0; i < input.data.size(); ++i) {
+        output.data[i] = std::max(0.0f, input.data[i]);
+    }
